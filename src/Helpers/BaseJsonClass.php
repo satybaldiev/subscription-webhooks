11a3d53af5f5ba30d8973effd00894dcf8179d9a
satybaldiev <daniyar.satybaldiev@axelkg.com>
2022-12-14 16:10:38 +0600

    added toArray() method to BaseJsonClass.php

--- src/Helpers/BaseJsonClass.php
+++ src/Helpers/BaseJsonClass.php
@@ -14,4 +14,32 @@ class BaseJsonClass implements JsonSerializable
         }
         return $json;
     }
+    function toArray()
+    {
+        return $this->object_to_array($this);
+    }
+
+    private function object_to_array($obj)
+    {
+        if (is_object($obj)) $obj = (array)$this->dismount($obj);
+        if (is_array($obj)) {
+            $new = array();
+            foreach ($obj as $key => $val) {
+                $new[$key] = $this->object_to_array($val);
+            }
+        } else $new = $obj;
+        return $new;
+    }
+
+    private function dismount($object)
+    {
+        $reflectionClass = new \ReflectionClass(get_class($object));
+        $array           = array();
+        foreach ($reflectionClass->getProperties() as $property) {
+            $property->setAccessible(true);
+            $array[$property->getName()] = $property->getValue($object);
+            $property->setAccessible(false);
+        }
+        return $array;
+    }
 }
